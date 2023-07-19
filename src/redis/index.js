551ce7ac3856f390879ec4e7f32e1e201ba1a4a6
nampgn <nampham18082003@gmail.com>
2023-07-19 20:49:32 +0700

    be-ud

--- src/redis/index.js
+++ src/redis/index.js
@@ -8,7 +8,8 @@ const redisClient = new Redis({
 // const redisClient = new Redis({
 //   port: 6379,
 //   host: "127.0.0.1",
-// });
+// }); 
+//
 
 redisClient.on("connect", () => {
   console.log("Connected to Redis");
