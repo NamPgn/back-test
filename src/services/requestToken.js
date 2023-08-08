6fdc1b89af4e19e53e505847ea097e54f7b8f760
nampgn <nampham18082003@gmail.com>
2023-08-08 15:31:36 +0700

    fe-ud

--- src/services/requestToken.js
+++ src/services/requestToken.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken"
 export const generateToken = (payload) => {
-    const token = jwt.sign(payload, "nampg", { expiresIn: '24h' })
+    const token = jwt.sign(payload, "nampg", { expiresIn: '10h' });
     return token;
 }
 export const verifyToken = (data) => {
